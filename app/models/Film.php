0613f803c25aff5fa7fb15fcd2c1327d26b5c8a6
mnans <nans.moll@ynov.com>
2026-02-22 15:28:26 +0100

    securité xss securité sql

--- app/models/Film.php
+++ app/models/Film.php
@@ -1,24 +1,31 @@
 <?php
-class Film {
-        private $pdo;
+class Film
+{
+    private $pdo;
 
-        public function __construct() {
-            require __DIR__ . "/database.php";
-            $this->pdo = $pdo;
-        }
+    public function __construct()
+    {
+        require __DIR__ . "/database.php";
+        $this->pdo = $pdo;
+    }
 
-        public function getOneFilm() {
-            $sql = "SELECT * FROM Film ORDER BY id_film DESC LIMIT 1";
-            $stmt = $this->pdo->query($sql);
-            return $stmt->fetch(PDO::FETCH_ASSOC);
-        }
-        public function getAll(){
-            $sql = "SELECT * FROM film";
-            $stmt = $this->pdo->query($sql);
-            return $stmt->fetchAll(PDO::FETCH_ASSOC);
-        }
+    public function getOneFilm()
+    {
+        $sql = "SELECT * FROM Film ORDER BY id_film DESC LIMIT 1";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+    public function getAll()
+    {
+        $sql = "SELECT * FROM film";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 
-    public function getFilmById($id) {
+    public function getFilmById($id)
+    {
         $sql = "SELECT * FROM film WHERE id_film = :id";
 
         $stmt = $this->pdo->prepare($sql);
